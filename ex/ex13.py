d7ec6b12803e3fa6980e810a94565185d04b47a0
Jelani Woods <jelani@firstdraft.com>
2019-01-11 11:58:47 -0600

    wip

---
+++ ex/ex13.py
@@ -0,0 +1,8 @@
+# learn how to pass variables to a script (a python file)
+from sys import argv
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
