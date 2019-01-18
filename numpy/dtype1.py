f3a2ceda0965a0c5119f90f3039fea535a8358c6
sun <jmw686@nyu.edu>
2019-01-18 16:30:08 -0600

    wip

---
+++ numpy/dtype1.py
@@ -0,0 +1,29 @@
+# using array-scalar type
+import numpy as np
+
+# ex 1
+# dt = np.dtype(np.int32)
+# print(dt)
+
+#ex3
+#int8, int16, int32, int64 can be replaced by == string 'i1', 'i2','i4', etc.
+# dt = np.dtype('>i4')
+# print(dt)
+
+#ex4
+#  the field name and the corresponding scalar data type is to be declared
+# dt = np.dtype([('age',np.int8)])
+# print(dt)
+
+# ex5
+# dt = np.dtype([('age',np.int8)])
+# a = np.array([(10,),(20,),(30,)], dtype = dt)
+# print(a)
+
+#ex6
+# file name can be used to access content of age column
+# dt = np.dtype([('age',np.int8)])
+# a  = np.array([(10,),(20,),(30,)], dtype = dt)
+# print(a['age'])
+
+# ex7
