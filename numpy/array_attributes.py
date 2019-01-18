eedfaaa0107384d7c2ca7bd152cebe5369237e8b
sun <jmw686@nyu.edu>
2019-01-18 16:48:33 -0600

    wip

---
+++ numpy/array_attributes.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+#ex1
+# a = np.array([[1,2,3],[4,5,6]])
+# print(a.shape)
+
+#ex2
+# a = np.array([[1,2,3],[4,5,6]])
+# a.shape = (3,2)
+# print(a)
+
+#ex3
+# a = np.array([[1,2,3],[4,5,6]])
+# b = a.reshape(3,2)
+# print(b)
+
+#ex4
+# an array of evenly spaced numbers
+# a = np.arange(24)
+# print(a)
+
+#ex5
+# a = np.arange(24)
+# print(a.ndim)
+# # now reshape it
+# b = a.reshape(2,4,3)
+# print(b)
+# # b is having three dimensions
+# print(b.ndim)
