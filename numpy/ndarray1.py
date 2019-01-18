648f101907325fc3f2258df3507c8953d7d1751e
sun <jmw686@nyu.edu>
2019-01-18 16:10:01 -0600

    wip

--- numpy/ndarray1.py
+++ numpy/ndarray1.py
@@ -3,6 +3,10 @@ import numpy as np
 # a = np.array([1, 2, 3])
 #
 # print(a)
+# ex2
+# a = np.array([[1,2], [3, 4]])
+# print(a)
 
-a = np.array([[1,2], [3, 4]])
+# minimum dimensions? don't really get it yet
+a = np.array([1, 2, 3,4,5], ndmin = 2)
 print(a)
